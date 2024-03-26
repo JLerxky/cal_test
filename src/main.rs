4df89a8c272fb5bdf1f726f0fa55d237645c4f47
JLer <jlerxky@live.com>
2024-03-26 16:29:29 +0800

    cal test

---
+++ src/main.rs
@@ -0,0 +1,236 @@
+#[macro_use]
+extern crate tracing;
+
+mod request;
+
+use std::sync::Arc;
+
+use clap::Parser;
+use color_eyre::{eyre::eyre, Result};
+use common_x::{configure, log};
+use indicatif::ProgressBar;
+use parking_lot::RwLock;
+use request::Request;
+use serde_json::Value;
+
+use crate::request::Job;
+
+fn clap_about() -> String {
+    let name = env!("CARGO_PKG_NAME").to_string();
+    let version = env!("CARGO_PKG_VERSION");
+    let authors = env!("CARGO_PKG_AUTHORS");
+    name + " " + version + "\n" + authors
+}
+
+#[derive(Parser)]
+#[clap(version, about = clap_about())]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Parser)]
+enum SubCommand {
+    #[clap(name = "run")]
+    Run(RunOpts),
+}
+
+/// A subcommand for run
+#[derive(Parser, Debug)]
+struct RunOpts {
+    #[clap(short = 'w', default_value = "0")]
+    worker: usize,
+    #[clap(short = 'c', default_value = "1")]
+    concurrency: u64,
+    #[clap(short = 't', default_value = "0")]
+    total: u64,
+    #[clap(short = 'g', default_value = "50")]
+    granularity: u64,
+    #[clap(short = 'j', default_value = "job.toml")]
+    job: String,
+    #[clap(short = 'd', default_value = "false")]
+    debug: bool,
+}
+
+fn main() {
+    ::std::env::set_var("RUST_BACKTRACE", "full");
+
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Run(opts) => {
+            if opts.debug {
+                log::init_log_filter("debug,hyper=info");
+            } else {
+                log::init_log_filter("info");
+            }
+            let runtime = if opts.worker > 0 {
+                tokio::runtime::Builder::new_multi_thread()
+                    .worker_threads(opts.worker)
+                    .enable_all()
+                    .build()
+                    .unwrap()
+            } else {
+                tokio::runtime::Builder::new_current_thread()
+                    .enable_all()
+                    .build()
+                    .unwrap()
+            };
+            if let Err(e) = runtime.block_on(run(opts)) {
+                error!("err: {:?}", e);
+            }
+        }
+    }
+}
+
+#[inline]
+fn categorize(elapsed_vec: &mut Vec<usize>, granularity: u64, elapsed: f64) {
+    let i = (elapsed as u64 / granularity) as usize;
+    if i >= elapsed_vec.len() {
+        for _ in 0..=(i - elapsed_vec.len()) {
+            elapsed_vec.push(0);
+        }
+    }
+    elapsed_vec[i] += 1;
+}
+
+async fn run(opts: RunOpts) -> Result<()> {
+    let total = opts.total;
+    let pb = ProgressBar::new(total);
+    let record = Arc::new(RwLock::new(Record {
+        success: 0,
+        elapsed_sum: 0.0,
+        elapsed_vec: vec![],
+    }));
+
+    let (tx, rv) = flume::bounded(total as usize);
+
+    let job: Job = configure::file_config(&opts.job).unwrap();
+    for i in 0..opts.total {
+        let request = job.get(i);
+        tx.send(request).ok();
+    }
+
+    let mut workers = vec![];
+
+    let begin_time = std::time::SystemTime::now();
+
+    for _ in 0..opts.concurrency {
+        let record = record.clone();
+        let rv = rv.clone();
+        let pb = pb.clone();
+        let granularity = opts.granularity;
+        let worker = tokio::spawn(async move {
+            while let Ok(key) = rv.try_recv() {
+                if let Err(e) = subtask(key, record.clone(), granularity).await {
+                    warn!("task err: {:?}", e);
+                }
+                pb.inc(1);
+            }
+        });
+        workers.push(worker);
+    }
+
+    for worker in workers {
+        worker.await.ok();
+    }
+    pb.finish_with_message("done");
+
+    let elapsed_total = begin_time.elapsed().unwrap().as_secs_f64();
+    let record = record.read();
+    let elapsed_avg = (record.elapsed_sum / record.elapsed_num() as f64) * 1000.;
+    let speed = total as f64 / elapsed_total;
+    for (i, num) in record.elapsed_vec.iter().enumerate() {
+        if *num > 0 {
+            info!(
+                "elapsed [{}, {}) ms: {num}",
+                opts.granularity * i as u64,
+                opts.granularity * (i + 1) as u64,
+            );
+        }
+    }
+    info!("elapsed_avg: {elapsed_avg} ms");
+    info!("elapsed_total: {elapsed_total} s");
+    info!("speed: {speed} /s");
+    info!(
+        "total: {total}, success: {}, failed: {}",
+        record.success,
+        total - record.success
+    );
+
+    Ok(())
+}
+
+struct Record {
+    success: u64,
+    elapsed_sum: f64,
+    elapsed_vec: Vec<usize>,
+}
+
+impl Record {
+    #[inline]
+    pub fn elapsed_num(&self) -> usize {
+        self.elapsed_vec.iter().sum()
+    }
+}
+
+#[inline]
+fn recording(tr: TaskResult, record: Arc<RwLock<Record>>, granularity: u64) {
+    let mut record = record.write();
+    if tr.status_code == 200 {
+        debug!("completed: {tr:?}");
+        record.success += 1;
+    } else {
+        warn!("task err: {tr:?}");
+    }
+    record.elapsed_sum += tr.elapsed;
+    categorize(&mut record.elapsed_vec, granularity, tr.elapsed * 1000.0);
+}
+
+#[derive(Debug)]
+pub struct TaskResult {
+    pub req: Request,
+    pub status_code: u16,
+    pub elapsed: f64,
+    pub rsp: String,
+}
+
+#[inline]
+async fn subtask(request: Request, record: Arc<RwLock<Record>>, granularity: u64) -> Result<()> {
+    let http_client = reqwest::Client::builder()
+        .connect_timeout(std::time::Duration::from_secs(10))
+        .timeout(std::time::Duration::from_secs(10))
+        .build()?;
+
+    let begin_time = std::time::SystemTime::now();
+    let mut req_builder = http_client.post(&request.url).json(&request.body);
+
+    if let Some(headers) = request.headers.as_object().cloned() {
+        for header in headers {
+            req_builder = req_builder.header(header.0, header.1.as_str().unwrap());
+        }
+    }
+
+    let rsp = req_builder
+        .send()
+        .await
+        .map_err(|e| eyre!("send {request:?} err: {e}"))?;
+
+    let elapsed = begin_time.elapsed().unwrap().as_secs_f64();
+    let status_code = rsp.status().as_u16();
+    let json = rsp
+        .json::<Value>()
+        .await
+        .map_err(|e| eyre!("decode json err {request:?}: {e}"))?;
+    recording(
+        TaskResult {
+            req: request,
+            status_code,
+            elapsed,
+            rsp: json.to_string(),
+        },
+        record,
+        granularity,
+    );
+    Ok(())
+}
---
+++ src/request.rs
@@ -0,0 +1,71 @@
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[serde(default)]
+pub struct Job {
+    pub url: String,
+    pub headers: Value,
+    pub body: Value,
+    pub params: Value,
+    pub init_seq_num: u64,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[serde(default)]
+pub struct Request {
+    pub url: String,
+    pub headers: Value,
+    pub body: Value,
+}
+
+impl Job {
+    #[inline]
+    pub fn get(&self, index: u64) -> Request {
+        let request = Request {
+            url: self.url.clone(),
+            headers: self.headers.clone(),
+            body: self.body.clone(),
+        };
+
+        let request_json = serde_json::to_string(&request).unwrap();
+        let mut result_json = request_json.clone();
+        let re = Regex::new(r"<([^']+)>").unwrap();
+        for gen in re.captures_iter(&request_json) {
+            let (full, [gen_str]) = gen.extract();
+            let gen = gen_str.split(':').collect::<Vec<&str>>();
+            let name = gen[0];
+            let gen_type = gen[1];
+            let param = self.params.get(name);
+            match gen_type {
+                "SeqNum" => {
+                    let (num, step) = if let Some(seq_param) = param {
+                        let num = &seq_param["init_seq_num"];
+                        let step = &seq_param["step"];
+                        (
+                            num.as_u64().unwrap_or(self.init_seq_num),
+                            step.as_u64().unwrap_or(1),
+                        )
+                    } else {
+                        (self.init_seq_num, 1)
+                    };
+                    let value = num + (step * index);
+                    result_json = result_json.replace(full, &value.to_string());
+                }
+                "UUID" => {}
+                _ => {}
+            }
+        }
+        serde_json::from_str(&result_json).unwrap()
+    }
+}
+
+#[test]
+fn test_job_next() {
+    common_x::log::init_log_filter("info");
+    let job: Job = common_x::configure::file_config("job.toml").unwrap();
+    let request = job.get(1);
+    info!("request: {request:?}");
+    info!("job: {job:?}");
+}
